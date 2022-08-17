fcab03ad4befb8fc820c743bb1a160ed3ac10cd0
MoSangBin <mosangbin94@gmail.com>
2022-08-17 10:50:18 +0900

    fix: course data 수정

--- src/utils/courseData.ts
+++ src/utils/courseData.ts
@@ -1,37 +1,37 @@
 export const courseData = [
   {
     id: 1,
-    url: "https://urclass.codestates.com/static/media/cover.23e06f01.jpg",
+    url: "https://urclass.codestates.com/static/media/img-cover.23e06f01.jpg",
     title: "[SEB FE] Section 1",
     body: "웹 프론트엔드 개발의 기초지식을 기반으로 스스로 단순한 Web App을 만들 수 있다.",
   },
   {
     id: 2,
-    url: "https://urclass.codestates.com/static/media/cover.23e06f01.jpg",
+    url: "https://urclass.codestates.com/static/media/img-cover.23e06f01.jpg",
     title: "[SEB FE] Section 2",
     body: "서버와 통신이 가능한 구조적인 Web App을 만들 수 있다.",
   },
   {
     id: 3,
-    url: "https://urclass.codestates.com/static/media/cover.23e06f01.jpg",
+    url: "https://urclass.codestates.com/static/media/img-cover.23e06f01.jpg",
     title: "[SEB FE] Section 3",
     body: "사용자 친화적이고 안전한 Web App을 만들 수 있다.",
   },
   {
     id: 4,
-    url: "https://urclass.codestates.com/static/media/cover.23e06f01.jpg",
+    url: "https://urclass.codestates.com/static/media/img-cover.23e06f01.jpg",
     title: "[SEB FE] Section 4",
     body: "사람과 기계가 모두 쉽고 빠르게 접근 가능한 Web App을 만들 수 있다.",
   },
   {
     id: 5,
-    url: "https://urclass.codestates.com/static/media/cover.23e06f01.jpg",
+    url: "https://urclass.codestates.com/static/media/img-cover.23e06f01.jpg",
     title: "[SEB FE] Project",
     body: "First Project, Final Project",
   },
   {
     id: 6,
-    url: "https://urclass.codestates.com/static/media/cover.23e06f01.jpg",
+    url: "https://urclass.codestates.com/static/media/img-cover.23e06f01.jpg",
     title: "[SEB FE] Wiki & Survey",
     body: "SEB FE 부트캠프에서 활용되고 있는 Wiki & Survey",
   },
