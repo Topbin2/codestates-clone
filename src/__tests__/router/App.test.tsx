c2fd65b4d4a1e7b325bcc0a251d0078b1dda5273
MoSangBin <mosangbin94@gmail.com>
2022-08-17 12:20:31 +0900

    test: route 테스트 추가

--- src/__tests__/router/App.test.tsx
+++ src/__tests__/router/App.test.tsx
@@ -1,10 +1,29 @@
 import App from "../../App";
-import { render, screen } from "../../utils/test-utils";
+import { getAllByRole, render, screen } from "../../utils/test-utils";
 
 describe("route", () => {
-
-  it("'/' path는 Main 페이지가 렌더링 된다.", () => {
-    render(<App />);
+  it("'/' path는 Main 컴포넌트가 렌더링 된다.", () => {
+    render(<App />, { route: "/" });
     expect(screen.getByText(/magazine/i)).toBeInTheDocument();
+  });
+
+  it("'/coplit' path는 Coplit 컴포넌트가 렌더링 된다.", () => {
+    const { container } = render(<App />, { route: "/coplit" });
+    const result = container.querySelector(".lds-ring");
+    expect(result).toBeInTheDocument();
+  });
+
+  it("'/job' path는 Job 컴포넌트가 렌더링 된다.", () => {
+    const { container } = render(<App />, { route: "/job" });
+    const result = container.querySelector(".lds-ring");
+    expect(result).toBeInTheDocument();
+  });
+
+  it("'/course' path는 Course 컴포넌트가 렌더링 된다.", () => {
+    render(<App />, { route: "/course" });
+    const tabs = screen.getAllByRole("link");
+    expect(tabs[1]).toHaveTextContent("수강 중인 코스");
+    expect(tabs[2]).toHaveTextContent("개강 예정 코스");
+    expect(tabs[3]).toHaveTextContent("만료된 코스");
   });
 });
